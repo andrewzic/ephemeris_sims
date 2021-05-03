2ab8ef3fe692fddb8440f0c9a85d1e3c771fdeb2
Andrew Zic <andrew.john.zic@gmail.com>
2021-05-03 15:02:05 +1000

    added informed_sample func to run_analysis script

--- run_analysis.py
+++ run_analysis.py
@@ -20,6 +20,42 @@ custom = ppta_dr2_models.PPTADR2Models
 params = enterprise_warp.Params(opts.prfile,opts=opts,custom_models_obj=custom)
 pta = enterprise_warp.init_pta(params)
 
+def informed_sample(hypermodel, noisedict):
+
+
+    """
+    Yanked from Boris' enterprise_extensions fork.
+    Originally was a method of hypermodel class
+    now takes a hypermodel object as arg
+    
+    Take an initial sample from the noise file. Abscent parameters will be
+    sampled from the prior.
+    """
+
+    x0 = [np.array(p.sample()).ravel().tolist() \
+          if p.name not in noisedict.keys() \
+          else np.array(noisedict[p.name]).ravel().tolist() \
+          for p in hypermodel.models[0].params]
+    uniq_params = [str(p) for p in hypermodel.models[0].params]
+
+    for model in hypermodel.models.values():
+            param_diffs = np.setdiff1d([str(p) for p  in model.params], \
+                                        uniq_params)
+            mask = np.array([str(p) in param_diffs for p in model.params])
+            x0.extend([np.array(pp.sample()).ravel().tolist() \
+                       if pp.name not in noisedict.keys() \
+                       else np.array(noisedict[pp.name]).ravel().tolist() \
+                       for pp in np.array(model.params)[mask]])
+
+            uniq_params = np.union1d([str(p) for p in model.params], \
+                                      uniq_params)
+
+    x0.extend([[0.1]])
+            
+    return np.array([p for sublist in x0 for p in sublist])
+
+
+
 if params.sampler == 'ptmcmcsampler':
     super_model = hypermodel.HyperModel(pta)
     print('Super model parameters: ', super_model.params)
@@ -43,7 +79,9 @@ if params.sampler == 'ptmcmcsampler':
     noisedict = get_noise_dict(psrlist=[pp.name for pp in params.psrs],
                                noisefiles=params.noisefiles)
     print('THIS IS NOISE DICT', noisedict)
-    x0 = super_model.informed_sample(noisedict)
+
+    x0 = informed_sample(super_model, noisedict)
+    #x0 = super_model.informed_sample(noisedict)
     # Start ORF inference with zero correlation:
     if 'corr_coeff_0' in super_model.param_names and \
        len(x0)==len(super_model.param_names):
@@ -63,7 +101,7 @@ if params.sampler == 'ptmcmcsampler':
     #import ipdb; ipdb.set_trace()
     if opts.mpi_regime != 1:
       sampler.sample(x0, N, **upd_sample_kwargs)
-      
+
     else:
       print('Preparations for the MPI run are complete - now set \
              opts.mpi_regime to 2 and enjoy the speed!')
